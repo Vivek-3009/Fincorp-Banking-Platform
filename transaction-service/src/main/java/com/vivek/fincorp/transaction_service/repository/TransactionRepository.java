81b4b022d3674e63007f9bae8c08f81771c4b2e9
Vivek <vivek.bisht555@gmail.com>
2026-01-21 23:18:08 +0530

    Added method in repository in transaction-service in Fincorp

--- transaction-service/src/main/java/com/vivek/fincorp/transaction_service/repository/TransactionRepository.java
+++ transaction-service/src/main/java/com/vivek/fincorp/transaction_service/repository/TransactionRepository.java
@@ -1,5 +1,6 @@
 package com.vivek.fincorp.transaction_service.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ import com.vivek.fincorp.transaction_service.entity.Transaction;
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
 
     Optional<Transaction> findByIdempotencyKey(String idempotencyKey);
+    List<Transaction> findByFromAccountNumberOrToAccountNumber(String fromAccount, String toAccount);
 }
