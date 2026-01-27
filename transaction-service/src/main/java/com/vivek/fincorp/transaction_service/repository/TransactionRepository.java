1a0f111630b49a188a1d5bffc5c279101d41069d
Vivek <vivek.bisht555@gmail.com>
2026-01-28 02:47:15 +0530

    Modified Transaction for Pageing in Transaction Service

--- transaction-service/src/main/java/com/vivek/fincorp/transaction_service/repository/TransactionRepository.java
+++ transaction-service/src/main/java/com/vivek/fincorp/transaction_service/repository/TransactionRepository.java
@@ -1,8 +1,8 @@
 package com.vivek.fincorp.transaction_service.repository;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.vivek.fincorp.transaction_service.entity.Transaction;
