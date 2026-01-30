75a2bf9861f935482317fa12a5ce04db1afb8702
Vivek <vivek.bisht555@gmail.com>
2026-01-31 02:10:05 +0530

    Modified Transaction entity in Transaction Service

--- transaction-service/src/main/java/com/vivek/fincorp/transaction_service/entity/Transaction.java
+++ transaction-service/src/main/java/com/vivek/fincorp/transaction_service/entity/Transaction.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 
 import com.vivek.fincorp.transaction_service.enums.TransactionStatus;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -12,7 +13,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,7 +23,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "transactions")
+@Table(
+    name = "transactions",
+    uniqueConstraints = @UniqueConstraint(columnNames = "idempotencyKey")
+)
 @Getter
 @Setter
 @Builder
@@ -32,20 +38,35 @@ public class Transaction {
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
+    @Column(nullable = false)
     private String fromAccountNumber;
+
+    @Column(nullable = false)
     private String toAccountNumber;
 
+    @Column(nullable = false)
     private BigDecimal amount;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private TransactionStatus status;
 
+    @Column(nullable = false, unique = true)
     private String idempotencyKey;
 
+    @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 
     @PrePersist
     void onCreate() {
         this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
     }
 }
