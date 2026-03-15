3be96a7f4e2cc8755ec798c809c394409425950b
Vivek <vivek.bisht555@gmail.com>
2026-03-16 00:13:51 +0530

    Added TransactionEventPublisher to Tranasction  service

--- transaction-service/src/main/java/com/vivek/fincorp/transaction_service/publisher/TransactionEventPublisher.java
+++ transaction-service/src/main/java/com/vivek/fincorp/transaction_service/publisher/TransactionEventPublisher.java
@@ -1,21 +1,21 @@
-// package com.vivek.fincorp.transaction_service.publisher;
+package com.vivek.fincorp.transaction_service.publisher;
 
-// import org.springframework.kafka.core.KafkaTemplate;
-// import org.springframework.stereotype.Component;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
 
-// import com.vivek.fincorp.transaction_service.event.TransactionCompletedEvent;
+import com.vivek.fincorp.transaction_service.event.TransactionCompletedEvent;
 
-// import lombok.RequiredArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-// @Component
-// @RequiredArgsConstructor
-// public class TransactionEventPublisher {
+@Component
+@RequiredArgsConstructor
+public class TransactionEventPublisher {
 
-//     private static final String TOPIC = "transaction-events";
+    private static final String TOPIC = "transaction-events";
 
-//     private final KafkaTemplate<String, TransactionCompletedEvent> kafkaTemplate;
+    private final KafkaTemplate<String, TransactionCompletedEvent> kafkaTemplate;
 
-//     public void publish(TransactionCompletedEvent event) {
-//         kafkaTemplate.send(TOPIC, event.transactionId(), event);
-//     }
-// }
+    public void publish(TransactionCompletedEvent event) {
+        kafkaTemplate.send(TOPIC, event.transactionId(), event);
+    }
+}
